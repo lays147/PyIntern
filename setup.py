f35f220549651a9e7b6cffa168d16c0f7f1d2193
Lays Rodrigues <lays.rodrigues@kde.org>
2018-06-24 22:28:40 -0300

    silly me?
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='PyIntern',
     version='0.1',
-    packages=find_packages(exclude='tests'),
+    packages=find_packages(),
     include_package_data=True,
     desciption='System to manage Interships',
     author='Lays Rodrigues',
