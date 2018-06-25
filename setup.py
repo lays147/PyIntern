8a558f1cfee645a80b91320c562a3be22345651b
Lays Rodrigues <lays.rodrigues@kde.org>
2018-06-24 21:49:37 -0300

    Add setup.py
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='PyIntern',
+    version='0.1',
+    packages=find_packages(exclude='tests'),
+    include_package_data=True,
+    desciption='System to manage Interships',
+    author='Lays Rodrigues',
+    author_email='lays.rodrigues@kde.org',
+    install_requires=[
+        'Django==2.0.4',
+        'django-taggit==0.22.2',
+    ])
