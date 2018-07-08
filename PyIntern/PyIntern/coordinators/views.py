7ecf100f843b3275ffdf69d09c5d40923d16c866
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-08 13:45:28 -0300

    Add view functions to handle new registration
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/coordinators/views.py
+++ PyIntern/PyIntern/coordinators/views.py
@@ -1,6 +1,8 @@
 """views for coordinators module."""
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url as r
+from django.http import HttpResponseRedirect, HttpResponse
 from PyIntern.users.models import Coordinator
+from .forms import CoordinatorsForm
 
 
 # Create your views here.
@@ -21,3 +23,32 @@ def list_coordinators(request):
         'coordinators_list.html',
         {'coordinators': coordinators},
     )
+
+
+def new_register(request):
+    """Handle request to the form."""
+    if request.method == 'POST':
+        return create(request)
+    return empty_form(request)
+
+
+def empty_form(request):
+    """Crete empty form."""
+    return render(
+        request,
+        'coordinators_form.html',
+        {'form': CoordinatorsForm()},
+    )
+
+
+def create(request):
+    """Submit new form."""
+    form = CoordinatorsForm(request.POST)
+    if not form.is_valid():
+        return render(
+            request,
+            'coordinators_form.html',
+            {'form': form},
+        )
+    Coordinator.objects.create(**form.cleaned_data)
+    return HttpResponseRedirect(r('list_coordinators'))
