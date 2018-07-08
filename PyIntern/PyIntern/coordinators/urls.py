2da928df80a8ae41925a07fe88fb6ff4f918b38e
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-08 13:46:00 -0300

    Add urls for coordinator forms
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/coordinators/urls.py
+++ PyIntern/PyIntern/coordinators/urls.py
@@ -1,8 +1,9 @@
 """urls for coordinators."""
 from django.urls import path
-from .views import home, list_coordinators
+from .views import home, list_coordinators, new_register
 
 urlpatterns = [
     path('', home),
-    path('list', list_coordinators, name='list_coordinators'),
+    path('list/', list_coordinators, name='list_coordinators'),
+    path('register/', new_register, name='new_coordinator')
 ]
