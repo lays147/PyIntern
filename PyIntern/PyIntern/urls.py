f1253550a3a5a4e31b7afe2b151d2fbbf1d73916
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-08 10:13:18 -0300

    Include coordinators urls on main urls file.
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/urls.py
+++ PyIntern/PyIntern/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from PyIntern.core import views
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('coordinators/', include('PyIntern.coordinators.urls')),
 ]
