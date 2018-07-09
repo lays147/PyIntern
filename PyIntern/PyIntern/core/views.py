f93b1e96134a02f6ec01e6485034e00f8d0f2db3
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-09 17:50:53 -0300

    Implement login for admin/coordinators
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/core/views.py
+++ PyIntern/PyIntern/core/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+"""views for core."""
+from django.shortcuts import render, resolve_url as r
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def home(request):
     """Return home page."""
+    if request.method == "POST":
+        return log_user(request)
     return render(request, 'index.html')
+
+
+def log_user(request):
+    """Auth the user."""
+    user = request.POST.get('user')
+    password = request.POST.get('pwd')
+    auth = authenticate(request, username=user, password=password)
+    if auth is None:
+        return render(request, 'index.html', {'error': True})
+    else:
+        login(request, auth)
+        return HttpResponseRedirect(r('coordinators_home'))
