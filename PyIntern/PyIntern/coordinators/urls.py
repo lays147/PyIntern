3aa817845e1ce811f70491add4a3d1407f85778f
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-09 14:21:52 -0300

    Add name to coordinators home url
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/coordinators/urls.py
+++ PyIntern/PyIntern/coordinators/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import home, list_coordinators, new_register
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='coordinators_home'),
     path('list/', list_coordinators, name='list_coordinators'),
     path('register/', new_register, name='new_coordinator')
 ]
