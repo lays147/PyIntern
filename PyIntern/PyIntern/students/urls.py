628241ad32255fb725e407f982f8fcfc1ef94d14
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-10 00:30:27 -0300

    Connect coordinators home to students list
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/students/urls.py
+++ PyIntern/PyIntern/students/urls.py
@@ -4,6 +4,6 @@ from .views import home, list_students, new_register
 
 urlpatterns = [
     path('', home, name='students_home'),
-    path('list/', list_students, name='students_list'),
+    path('list/', list_students, name='list_students'),
     path('register/', new_register, name='new_student'),
 ]
