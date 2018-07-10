1155d13837368882c9d64acc45e30351861bff50
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-09 23:52:13 -0300

    Add clean method on students to validate passwords
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/students/forms.py
+++ PyIntern/PyIntern/students/forms.py
@@ -6,6 +6,15 @@ from PyIntern.users.models import Student
 class StudentsForm(forms.Form):
     """Students Form."""
 
+    def clean(self):
+        """Clean form."""
+        cleaned_data = super().clean()
+        pw1 = cleaned_data.get('password1')
+        pw2 = cleaned_data.get('password2')
+        if pw1 != pw2:
+            self.add_error('password2', 'Senhas não conferem.')
+        return cleaned_data
+
     registration = forms.CharField(
         label='Matrícula',
         required=True,
@@ -21,6 +30,16 @@ class StudentsForm(forms.Form):
     username = forms.CharField(
         label='Nome de Usuário',
         required=True,
+    )
+    password1 = forms.CharField(
+        label='Senha',
+        required=True,
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label='Confirme sua senha',
+        required=True,
+        widget=forms.PasswordInput,
     )
     phone = forms.CharField(
         label='Telefone',
