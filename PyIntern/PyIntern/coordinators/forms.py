b54e3997d81abab0affb2b50363ac045dcddb07f
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-10 00:28:20 -0300

    Add clean method on coordinators to validate passwords
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/coordinators/forms.py
+++ PyIntern/PyIntern/coordinators/forms.py
@@ -5,6 +5,15 @@ from django import forms
 class CoordinatorsForm(forms.Form):
     """Coordinators Form."""
 
+    def clean(self):
+        """Clean form."""
+        cleaned_data = super().clean()
+        pw1 = cleaned_data.get('password1')
+        pw2 = cleaned_data.get('password2')
+        if pw1 != pw2:
+            self.add_error('password2', 'Senhas não conferem.')
+        return cleaned_data
+
     registration = forms.CharField(
         label='Matrícula',
         required=True,
@@ -20,6 +29,16 @@ class CoordinatorsForm(forms.Form):
     username = forms.CharField(
         label='Nome de Usuário',
         required=True,
+    )
+    password1 = forms.CharField(
+        label='Senha',
+        required=True,
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label='Confirme sua senha',
+        required=True,
+        widget=forms.PasswordInput,
     )
     phone = forms.CharField(
         label='Telefone',
