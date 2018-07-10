f577d9f12e68f571bf3ee932976ee373beef56c7
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-10 00:06:22 -0300

    Add clean method on company to validate passwords
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/companies/forms.py
+++ PyIntern/PyIntern/companies/forms.py
@@ -5,6 +5,15 @@ from django import forms
 class CompanyForm(forms.Form):
     """Company Form."""
 
+    def clean(self):
+        """Clean form."""
+        cleaned_data = super().clean()
+        pw1 = cleaned_data.get('password1')
+        pw2 = cleaned_data.get('password2')
+        if pw1 != pw2:
+            self.add_error('password2', 'Senhas não conferem.')
+        return cleaned_data
+
     company_name = forms.CharField(
         label='Nome da Empresa',
         max_length=200,
@@ -32,6 +41,16 @@ class CompanyForm(forms.Form):
     username = forms.CharField(
         label='Nome de Usuário',
         required=True,
+    )
+    password1 = forms.CharField(
+        label='Senha',
+        required=True,
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label='Confirme sua senha',
+        required=True,
+        widget=forms.PasswordInput,
     )
     phone = forms.CharField(
         label='Telefone',
