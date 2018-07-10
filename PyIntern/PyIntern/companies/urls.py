d8af900e15f491905554650763490963d4a5b520
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-10 00:07:41 -0300

    Improve company form template
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/companies/urls.py
+++ PyIntern/PyIntern/companies/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import home, new_register, list_companies
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='company_home'),
     path('register/', new_register, name='new_company'),
     path('list/', list_companies, name='list_companies')
 ]
