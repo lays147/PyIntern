b4fc3082f8552d96e1133729c12a5cf9d57b89b3
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-13 13:06:24 -0300

    Django Admin Model for Student
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/students/admin.py
+++ PyIntern/PyIntern/students/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from PyIntern.users.models import Student
 
 # Register your models here.
+
+
+class StudentModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'registration', 'allowed')
+    date_hierarchy = 'created_at'
+    search_fields = ('name', 'registration', 'cpf')
+
+
+admin.site.register(Student, StudentModelAdmin)
