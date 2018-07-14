bc17207c605bfb15f7d6e0aebe2f940303d6caca
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-14 16:31:40 -0300

    Add list jobs options
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/jobs/urls.py
+++ PyIntern/PyIntern/jobs/urls.py
@@ -1,7 +1,8 @@
 """urls for coordinators."""
-from django.urls import path
-from .views import list_jobs
+from django.urls import path, re_path
+from .views import list_jobs, get_job
 
 urlpatterns = [
     path('', list_jobs, name='list_jobs'),
+    re_path(r'^(\d+)/$', get_job, name='get_job'),
 ]
--- PyIntern/PyIntern/jobs/views.py
+++ PyIntern/PyIntern/jobs/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.forms.models import model_to_dict
+from django.shortcuts import render, get_object_or_404
 from .models import Jobs
 
 
@@ -7,9 +8,23 @@ from .models import Jobs
 @login_required
 def list_jobs(request):
     """Return list of jobs on the system."""
-    jobs = Jobs.objects.all()
+    jobs = Jobs.objects.all().filter(available=True)
     return render(
         request,
         'jobs_list.html',
         {'jobs': jobs},
+    )
+
+
+@login_required
+def get_job(request, id):
+    """Jobs file."""
+    job = get_object_or_404(Jobs, id=id)
+    data = model_to_dict(job)
+    return render(
+        request,
+        'view_job.html',
+        {
+            'job': data,
+        },
     )
