3392a488609971f05311823ad28a9f64d1d74afc
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-14 17:40:57 -0300

    Add view to student to register in a job
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/jobs/urls.py
+++ PyIntern/PyIntern/jobs/urls.py
@@ -1,8 +1,12 @@
 """urls for coordinators."""
 from django.urls import path, re_path
-from .views import list_jobs, get_job
+from .views import list_jobs, get_job, register_into_job
 
 urlpatterns = [
     path('', list_jobs, name='list_jobs'),
     re_path(r'^(\d+)/$', get_job, name='get_job'),
+    re_path(
+        r'^register/(?P<pk_job>\d+)',
+        register_into_job,
+        name='register_into_job')
 ]
--- PyIntern/PyIntern/jobs/views.py
+++ PyIntern/PyIntern/jobs/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.forms.models import model_to_dict
-from django.shortcuts import render, get_object_or_404
-from .models import Jobs
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, resolve_url as r, get_object_or_404
+from .models import Jobs, Candidatures
+from PyIntern.users.models import Student, Company
 
 
 # Create your views here.
@@ -21,12 +23,27 @@ def get_job(request, id):
     """Jobs file."""
     job = get_object_or_404(Jobs, id=id)
     data = model_to_dict(job)
+    data['company'] = model_to_dict(job.company)
     is_student = request.user.groups.filter(name='Students').exists()
     return render(
         request,
         'view_job.html',
         {
             'job': data,
-            'student': is_student
+            'student': is_student,
         },
     )
+
+
+@login_required
+def register_into_job(request, pk_job):
+    """Register into job."""
+    student = get_object_or_404(Student, username=request.user.username)
+    job = get_object_or_404(Jobs, id=pk_job)
+    company = get_object_or_404(Company, registration=job.company.registration)
+    Candidatures.objects.create(
+        student=student,
+        company=company,
+        job=job,
+    )
+    return HttpResponseRedirect(r('students_home'))
