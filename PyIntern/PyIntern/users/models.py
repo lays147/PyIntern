e6bb7b7250330e00185c4ff67b5821c28700a186
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-14 19:39:19 -0300

    Simplify user model + verbose name on fields
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/users/models.py
+++ PyIntern/PyIntern/users/models.py
@@ -1,20 +1,17 @@
+"""Abstract user model."""
 from django.db import models
 
 
 class User(models.Model):
     """User."""
 
-    registration = models.PositiveIntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    address = models.CharField(max_length=255)
-    username = models.CharField(max_length=20, unique=True)
+    name = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-Mail', unique=True)
+    phone = models.CharField('Telefone', max_length=20)
+    username = models.CharField('Nome de Usuário', max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        abstract = True
+        """Meta class for user."""
 
-    def __str__(self):
-        """Return user name."""
-        return self.name
+        abstract = True
