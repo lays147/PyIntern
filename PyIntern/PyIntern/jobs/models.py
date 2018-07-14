b75767811ab50c6e1cdb72c138e3b46db22afff4
Lays Rodrigues <lays.rodrigues@kde.org>
2018-07-14 20:12:52 -0300

    Add verbose name to jobs models
    Signed-off-by: Lays Rodrigues <lays.rodrigues@kde.org>

--- PyIntern/PyIntern/jobs/models.py
+++ PyIntern/PyIntern/jobs/models.py
@@ -5,22 +5,28 @@ from django.db import models
 class Jobs(models.Model):
     """Jobs Model."""
     company = models.ForeignKey(
-        'companies.Companies', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    requirements = models.CharField(max_length=500)
-    benefits = models.CharField(max_length=255)
+        'companies.Companies',
+        verbose_name='Empresa',
+        on_delete=models.CASCADE)
+    name = models.CharField('Nome da Vaga', max_length=100)
+    description = models.CharField('Descrição', max_length=500)
+    requirements = models.CharField('Requisitos', max_length=500)
+    benefits = models.CharField('Benefícios', max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
-    end_at = models.DateField()
-    answer_date = models.DateField()
-    available = models.BooleanField()
+    end_at = models.DateField('Data de Encerramento')
+    answer_date = models.DateField('Data de Retorno')
+    available = models.BooleanField('Inscrições Disponíveis')
 
 
 class Candidatures(models.Model):
     """Candidatures Model."""
 
     company = models.ForeignKey(
-        'companies.Companies', on_delete=models.CASCADE)
-    student = models.ForeignKey('students.Student', on_delete=models.CASCADE)
-    job = models.ForeignKey('Jobs', on_delete=models.CASCADE)
+        'companies.Companies',
+        verbose_name='Empresa',
+        on_delete=models.CASCADE)
+    student = models.ForeignKey(
+        'students.Student', verbose_name='Estudante', on_delete=models.CASCADE)
+    job = models.ForeignKey(
+        'Jobs', verbose_name='Vaga', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
